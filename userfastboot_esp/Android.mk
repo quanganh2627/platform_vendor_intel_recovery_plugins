9476b88d06522e3be4f27fc76ee5abc7ea6f69c1
Radivoje Jovanovic <radivoje.jovanovic@intel.com>
2014-01-09 15:59:54 -0800

    enable capsule update support over fastboot
    Issue:ADEV-489
    Change-Id: I3649dc7d6b130a1945a6aef68dc6d597a2ef0187
    Signed-off-by: Radivoje Jovanovic <radivoje.jovanovic@intel.com>

--- userfastboot_esp/Android.mk
+++ userfastboot_esp/Android.mk
@@ -6,7 +6,8 @@ LOCAL_SRC_FILES := ufb_esp.c
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
 LOCAL_C_INCLUDES := bootable/userfastboot \
-                    system/core/fs_mgr/include
+                    system/core/fs_mgr/include \
+                    bootable/recovery
 LOCAL_CFLAGS := -Wall -Werror -Wno-unused-parameter
 ifneq ($(DROIDBOOT_NO_GUI),true)
 LOCAL_CFLAGS += -DUSE_GUI
--- userfastboot_esp/ufb_esp.c
+++ userfastboot_esp/ufb_esp.c
@@ -10,12 +10,75 @@
 #include <userfastboot_plugin.h>
 #include <stdlib.h>
 #include <unistd.h>
+/*from bootable/recovery*/
+#include <bootloader.h>
 
 #include "fastboot.h"
 #include "userfastboot.h"
 
 #define MAXFILS 16
 #define BOOT_PART "bootloader"
+#define BOOTLOADER_CAPSULE_TEST_BCB "update-firmware"
+#define CAPSULE_TARGET_FOLDER "/mnt/bootloader/fwupdate"
+
+static int cmd_flash_capsule_update(Hashmap *params, int *fd, unsigned sz)
+{
+	struct fstab_rec *bootloaderVol, *miscVol;
+	void *data = NULL;
+	int ret;
+	static unsigned int fileNum;
+	struct bootloader_message msg;
+	char fileName[100];
+
+	memset (&msg, 0, sizeof(msg));
+	bootloaderVol = volume_for_path("/bootloader");
+	if (!bootloaderVol) {
+		pr_error("Couldn't find bootloader partition. Is your recovery.fstab valid?\n");
+		return -1;
+	}
+	if (mount_partition(bootloaderVol)) {
+		pr_error("Couldn't mount bootloader partition\n");
+		return -1;
+	}
+	if (fileNum == 0) {
+		ret = execute_command("rm -rf %s", CAPSULE_TARGET_FOLDER);
+		if (ret) {
+			pr_error("Failed to delete:%s\n", CAPSULE_TARGET_FOLDER);
+			goto err;
+		}
+	}
+	ret = mkdir(CAPSULE_TARGET_FOLDER, 0666);
+	if (ret && errno != EEXIST) {
+		pr_perror("mkdir");
+		goto err;
+	}
+
+	data = mmap64(NULL, sz, PROT_READ, MAP_SHARED, *fd, 0);
+	if (data == (void*)-1) {
+		pr_error("Failed to mmap the file\n");
+		goto err;
+	}
+
+	snprintf (fileName, sizeof(fileName), "%s/capsule%d.cap", CAPSULE_TARGET_FOLDER, fileNum++);
+	if (named_file_write(fileName, data, sz, 0, 0)) {
+		pr_error("Couldn't write capsule to bootloader partition.\n");
+		munmap(data, sz);
+		goto err;
+	}
+	munmap(data, sz);
+	miscVol = volume_for_path("/misc");
+	strncpy(msg.command, BOOTLOADER_CAPSULE_TEST_BCB, sizeof(msg.command));
+
+	if (named_file_write(miscVol->blk_device, (const unsigned char*) &msg, sizeof(msg), 0, 0)) {
+		pr_error("Failed to write bootloader message\n");
+		goto err;
+	}
+	unmount_partition(bootloaderVol);
+	return 0;
+err:
+	unmount_partition(bootloaderVol);
+	return -1;
+}
 
 static int rdline(char *ptr)
 {
@@ -113,4 +176,5 @@ out_fail:
 void libufb_esp_init(void)
 {
 	aboot_register_flash_cmd("bootloader", update_droid);
+	aboot_register_flash_cmd("capsule", cmd_flash_capsule_update);
 }
